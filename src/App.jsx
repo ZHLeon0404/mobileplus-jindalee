fd983aaa0e2eccbd3e3d35fa4f940b57b01d496d
ZHLeon0404 <143568431+ZHLeon0404@users.noreply.github.com>
2026-05-15 11:54:45 +1000

    change button to quote

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import emailjs from '@emailjs/browser';
 
 const deviceModels = {
   iPhone: [
+    'iPhone 17 Pro Max', 'iPhone 17 Pro', 'iPhone 17 Plus', 'iPhone 17',
     'iPhone 16 Pro Max', 'iPhone 16 Pro', 'iPhone 16 Plus', 'iPhone 16',
     'iPhone 15 Pro Max', 'iPhone 15 Pro', 'iPhone 15 Plus', 'iPhone 15',
     'iPhone 14 Pro Max', 'iPhone 14 Pro', 'iPhone 14 Plus', 'iPhone 14',
